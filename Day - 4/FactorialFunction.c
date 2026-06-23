ae11e186d75e451cdddb00a88c6bd1ae52314a9e
Shreya Singh <shreyasinghishu2005@gmail.com>
2026-06-23 17:14:01 +0530

    Add files via upload

---
+++ Day - 4/FactorialFunction.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int n) {
+int fact = 1;
+    int i;
+
+    for(i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial = %d", factorial(num));
+
+    return 0;
+}
