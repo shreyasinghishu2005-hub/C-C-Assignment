5c301f2407064187abcd067f246ac198f21ce01c
Shreya Singh <shreyasinghishu2005@gmail.com>
2026-06-23 17:14:36 +0530

    Add files via upload

---
+++ Day - 5/GCDFunction.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while(b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("GCD = %d", gcd(num1, num2));
+
+    return 0;
+}
